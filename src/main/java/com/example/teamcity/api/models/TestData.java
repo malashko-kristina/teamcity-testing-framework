d7b0484cc55a171ec61096691dcb34ccda020ee3
Kristina Malashko <kristina.malashko@pinch.nl>
2024-11-24 00:00:25 +0100

    Add some debugging things

--- src/main/java/com/example/teamcity/api/models/TestData.java
+++ src/main/java/com/example/teamcity/api/models/TestData.java
@@ -3,7 +3,7 @@ package com.example.teamcity.api.models;
 import lombok.Data;
 
 @Data
-public class TestData {
+public class TestData extends BaseModel {
     private Project project;
     private User user;
     private BuildType buildType;
